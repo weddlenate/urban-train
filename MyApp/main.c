7f02379c80b467b26e48624bc98e3ffb30beb54e
BoiseState Student <student@u.boisestate.edu>
2021-09-05 00:14:20 -0600

    Deeper Look Stuff

--- MyApp/main.c
+++ MyApp/main.c
@@ -2,7 +2,13 @@
 
 int main(void) {
 
-   /* Your code here */
+   int x = 0;
+
+   printf("Please enter a number: ");
+   scanf("%d", &x);
+
+   printf("The number that you entered was: %d\n", x);
+   printf("The address of the variable x is: %p\n", &x);
 
    return 0;
 }
